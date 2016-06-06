868d839c02040d784bbfe15f54d72be4624fc31b
Roma Storozhuk <roma.storoschuk@gmail.com>
2016-06-06 14:18:30 +0300

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -3,19 +3,19 @@
   any elements with the same value next to each other and preserving the original order of elements.
 */
 module.exports = function (input) {
- if (Array.isArray(input) !== true)
- {
+ if (Array.isArray(input) !== true) {
+   
    input = input.split('');
  }
- else
- {
-   for (var i = 0, j = 1, len = input.length; i < len; i++, j++)
-   {
-     if(input[i] === input[j]);
-     {
-       input.splice(j, 1)
+ else {
+   var output;
+   for (var i = 0, j = 1, len = input.length; i < len; i++, j++) {
+     
+     if(input[i] === input[j]) {
+       
+      output = input.splice(j, 1);
      }
    }
  }
- return input;
+ return output;
 };
